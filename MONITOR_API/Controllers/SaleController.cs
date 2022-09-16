547b9bdbf2d10453c9192190dac4faf735a0a6c8
Ryan O'Connor <93062352+OConnorRyan@users.noreply.github.com>
2022-09-16 03:37:28 -0400

    Initial Upload
    It compiles. Thats about it.

---
+++ MONITOR_API/Controllers/SaleController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MONITOR_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaleController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public SaleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select SALEID,SALENUMBER,PONUMBER,ORDERNUMBER from sale";
+
+            System.Data.DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DatabaseConnection");
+
+            MySqlDataReader myReader;
+            using(MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close(); 
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ MONITOR_API/Models/Sale.cs
@@ -0,0 +1,11 @@
+﻿namespace MONITOR_API.Models
+{
+    public class Sale
+    {
+        public int SALEID { get; set; }
+        public int SALENUMBER { get; set; }
+        public int PONUMBER { get; set; }
+        public int ORDERNUMBER { get; set; }
+
+    }
+}
